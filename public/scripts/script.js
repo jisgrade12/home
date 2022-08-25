ca6c71ee5ad25e66d38791091f2a62d0e6bd5185
Abos2024 <abos2024@gmail.com>
2022-08-25 23:20:30 +0300

    Script fixations

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,23 @@
-"use strict";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function typeWriter(el) {
-	const textArray = el.innerHTML.split("");
-	el.innerHTML = "";
-	textArray.forEach((letter, i) =>
-		setTimeout(() => (el.innerHTML += letter), 95 * i)
-	);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyC0xyTcV8cLK_ducPzzXJOCDXwtcuU8qOI",
+	authDomain: "jis-grade12.firebaseapp.com",
+	databaseURL:
+		"https://jis-grade12-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "jis-grade12",
+	storageBucket: "jis-grade12.appspot.com",
+	messagingSenderId: "189452419298",
+	appId: "1:189452419298:web:5dd56b80be401c58865ebb",
+	measurementId: "G-06NMN6Y708",
+};
 
-	setInterval(() => typeWriter(el), 8000);
-}
-
-typeWriter(elementEl);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
