167f78780741f13d4098e007d983e84dda813e7c
Abos2024 <abos2024@gmail.com>
2022-09-06 06:42:00 +0300

    Extra Pages

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,45 @@
-const signInBtn = document.getElementById("signIn");
-const signUpBtn = document.getElementById("signUp");
-const fistForm = document.getElementById("form1");
-const secondForm = document.getElementById("form2");
-const container = document.querySelector(".container");
-signInBtn.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
-});
-signUpBtn.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
-});
-fistForm.addEventListener("submit", (e) => e.preventDefault());
-secondForm.addEventListener("submit", (e) => e.preventDefault());
+(function () {
+	const second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
+
+	let today = new Date(),
+		dd = String(today.getDate()).padStart(2, "0"),
+		mm = String(today.getMonth() + 1).padStart(2, "0"),
+		yyyy = today.getFullYear(),
+		dayMonth = "09/30/",
+		birthday = dayMonth + yyyy;
+
+	today = mm + "/" + dd + "/" + yyyy;
+	if (today > birthday) {
+		birthday = dayMonth + nextYear;
+	}
+	//end
+
+	const countDown = new Date(birthday).getTime(),
+		x = setInterval(function () {
+			const now = new Date().getTime(),
+				distance = countDown - now;
+
+			(document.getElementById("days").innerText = Math.floor(distance / day)),
+				(document.getElementById("hours").innerText = Math.floor(
+					(distance % day) / hour
+				)),
+				(document.getElementById("minutes").innerText = Math.floor(
+					(distance % hour) / minute
+				)),
+				(document.getElementById("seconds").innerText = Math.floor(
+					(distance % minute) / second
+				));
+
+			//do something later when date is reached
+			if (distance < 0) {
+				document.getElementById("headline").innerText = "It's my birthday!";
+				document.getElementById("countdown").style.display = "none";
+				document.getElementById("content").style.display = "block";
+				clearInterval(x);
+			}
+			//seconds
+		}, 0);
+})();
