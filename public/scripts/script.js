d33661bbf66c3aceb2abef108b90d48b8a2a992b
Abos2024 <abos2024@gmail.com>
2022-09-06 18:35:12 +0300

    Update on pages [minor]

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -32,14 +32,11 @@
 				(document.getElementById("seconds").innerText = Math.floor(
 					(distance % minute) / second
 				));
-
-			//do something later when date is reached
 			if (distance < 0) {
 				document.getElementById("headline").innerText = "It's my birthday!";
 				document.getElementById("countdown").style.display = "none";
 				document.getElementById("content").style.display = "block";
 				clearInterval(x);
 			}
-			//seconds
 		}, 0);
 })();
