6712095f3659bd0091e837649682a033ca1aec52
Abos2024 <abos2024@gmail.com>
2022-09-11 19:11:19 +0300

    Minor update

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,5 +1,37 @@
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
+import { GoogleAuthProvider } from "firebase/auth";
+provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+import { getAuth } from "firebase/auth";
 
+const auth = getAuth();
+auth.languageCode = "it";
+provider.setCustomParameters({
+	login_hint: "user@example.com",
+});
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+	.then((result) => {
+		// This gives you a Google Access Token. You can use it to access the Google API.
+		const credential = GoogleAuthProvider.credentialFromResult(result);
+		const token = credential.accessToken;
+		// The signed-in user info.
+		const user = result.user;
+		// ...
+	})
+	.catch((error) => {
+		// Handle Errors here.
+		const errorCode = error.code;
+		const errorMessage = error.message;
+		// The email of the user's account used.
+		const email = error.customData.email;
+		// The AuthCredential type that was used.
+		const credential = GoogleAuthProvider.credentialFromError(error);
+		// ...
+	});
+
+const provider = new GoogleAuthProvider();
 import {
 	getAuth,
 	signInWithEmailAndPassword,
