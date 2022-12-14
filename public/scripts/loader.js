c450569effc422f552ce12bb19880b606c881770
Abos2024 <abos2024@gmail.com>
2022-12-14 19:11:41 +0300

    Minor

--- public/scripts/loader.js
+++ public/scripts/loader.js
@@ -1,16 +1,25 @@
 (function newFact() {
   var facts = [
-    "Mental Health is an important matter to discuss.",
-    "Mental health awareness protects us from consequences.",
-    "Better safe than sorry.",
-    "Mental health is very important.",
+    "Mental Health is an important matter to discuss",
+    "Mental health awareness protects us from consequences",
+    "Better safe than sorry",
+    "Mental health is very important",
+    "Spread Positivity",
+    "Spread Kindness",
+    "Help others",
+    "Be kind",
+    "Be meaningful",
+    "Advice other to good",
+    "Bring your colleagues up",
+    "Don't take mental health as a joke",
   ];
   var randomFact = Math.floor(Math.random() * facts.length);
   document.getElementById("factDisplay").innerHTML = facts[randomFact];
 })();
-const loader = document.getElementById("loader1");
-const page = document.getElementById("body1");
-(function loaderflow() {
+
+Window.onload = (function loaderflow() {
+  const loader = document.getElementById("loader1");
+  const page = document.getElementById("body1");
   if (loader.style.display === "flex") {
     page.style.overflow = "hidden";
   } else {
