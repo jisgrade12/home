c366b3caac4ad90b1522b296d148665049c76821
JIS Grade 12 <111739930+jisgrade12@users.noreply.github.com>
2022-12-14 13:00:27 +0300

    Loader update

--- public/scripts/loader.js
+++ public/scripts/loader.js
@@ -3,7 +3,7 @@
     "Mental Health is an important matter to discuss.",
     "Mental health awareness protects us from consequences.",
     "Better safe than sorry.",
-    "Better safe than sorry.",
+    "Mental health is very important.",
   ];
   var randomFact = Math.floor(Math.random() * facts.length);
   document.getElementById("factDisplay").innerHTML = facts[randomFact];
